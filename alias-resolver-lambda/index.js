67d09752455b0426ab08fd83d5ebb51c1590761f
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-10-20 18:03:14 -0600

    make alias resolver actually resolve aliases

--- alias-resolver-lambda/index.js
+++ alias-resolver-lambda/index.js
@@ -17,52 +17,19 @@
 
 "use strict";
 
-const req = require('request-promise-native');
-
-/*
-{
-    "Records": [
-        {
-            "cf": {
-                "config": {
-                    "distributionId": "EDFDVBD6EXAMPLE",
-                    "requestId": "MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE=="
-                },
-                "request": {
-                    "clientIp": "2001:0db8:85a3:0:0:8a2e:0370:7334",
-                    "method": "GET",
-                    "uri": "/picture.jpg",
-                    "querystring": "size=large",
-                    "headers": {
-                        "host": [
-                            {
-                                "key": "Host",
-                                "value": "d111111abcdef8.cloudfront.net"
-                            }
-                        ],
-                        "user-agent": [
-                            {
-                                "key": "User-Agent",
-                                "value": "curl/7.51.0"
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-    ]
-}
- */
+const fetch = require('node-fetch');
 
 const ALIAS_REGEX = /^\/(.*?)\/((?:(?:\d+\.(?:\d+|x)\.x)|latest|unstable))\//;
 
+let oldAliases;
+
 exports.handler = (event, context, callback) => {
+    console.log('Incoming Event', JSON.stringify(event, null, 2));
     let request = event.Records[0].cf.request;
 
     let uri = request.uri;
 
     console.log('Incoming request to', uri);
-    console.log('got headers', request.headers);
 
     let match = ALIAS_REGEX.exec(uri);
 
@@ -70,24 +37,77 @@ exports.handler = (event, context, callback) => {
         console.log('Not an alias; passing through');
         callback(null, request);
     } else {
-        console.log(`Is an alias, still passing through because we're lazy like that`);
-        callback(null, request);
+        let libId = match[1];
+        let aliasName = match[2];
+
+        console.log(`Appears to be an alias: ${libId}@${aliasName}; getting alias config`);
+        let host = request.headers.host[0].value;
+
+        let aliasConfigUrl = `https://${host}/.cdn-meta/aliases.json`;
+
+        console.log('Loading', aliasConfigUrl);
+
+        getAliasList(host).then(aliases => {
+            console.log('got aliases', aliases);
+
+            let lib = aliases[libId];
+            if (!lib) {
+                console.log(`No lib defined with id ${libId}; passing through`);
+                callback(null, request);
+                return;
+            }
+
+            let version = lib[aliasName];
+            if (!version) {
+                console.log(`No alias defined for ${libId}@${aliasName}; passing through`);
+                callback(null, request);
+                return;
+            }
+
+            let newUri = uri.replace(ALIAS_REGEX, `/$1/${version}/`);
+
+            console.log('Redirecting to', newUri);
+
+            let response = {
+                status: '302',
+                statusDescription: 'Found',
+                headers: {
+                    location: [{
+                        key: 'Location',
+                        value: newUri
+                    }],
+                    'cache-control': [{
+                        key: 'Cache-Control',
+                        value: 'public, max-age=300, s-maxage=300'
+                    }],
+                    'x-byu-cdn-alias-target': [{
+                        key: 'X-BYU-CDN-Alias-Target',
+                        value: version
+                    }],
+                }
+            };
+            callback(null, response);
+        }).catch(err => {
+            console.log('Got error', err);
+            callback(err);
+        });
     }
-
-
-
-    /*
-     * Generate HTTP redirect response with 302 status code and Location header.
-     */
-    // const response = {
-    //     status: '302',
-    //     statusDescription: 'Found',
-    //     headers: {
-    //         location: [{
-    //             key: 'Location',
-    //             value: 'http://docs.aws.amazon.com/lambda/latest/dg/lambda-edge.html',
-    //         }],
-    //     },
-    // };
-    // callback(null, response);
 };
+
+function getAliasList(host) {
+    let aliasConfigUrl = `https://${host}/.cdn-meta/aliases.json`;
+
+    console.log('Loading aliases from', aliasConfigUrl);
+
+    return fetch(aliasConfigUrl).then(response => {
+        let aliases = response.json();
+        oldAliases = aliases;
+        return aliases;
+    }).catch(err => {
+        if (oldAliases) {
+            console.error('Got error getting alias list, using old version', err);
+            return oldAliases;
+        }
+        throw err;
+    });
+}
