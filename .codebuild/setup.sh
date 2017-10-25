ff143a45c65d89cd31fcda098f079b182465c4d9
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-10-24 22:01:27 -0600

    add more logging

--- .codebuild/setup.sh
+++ .codebuild/setup.sh
@@ -2,13 +2,17 @@
 
 echo "Installing Nodejs and Yarn"
 
-apt-get update
-apt-get install apt-transport-https
+apt-get update -q
+apt-get install -yq apt-transport-https
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
-apt-get update && apt-get install -y nodejs yarn
+apt-get update -q && apt-get install -yq nodejs yarn
+
+echo "Installing lerna"
+
+yarn global add lerna
 
