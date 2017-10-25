567e1d2f267a21c650b2876e876c34611de2d1c4
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-10-24 21:14:07 -0600

    make setup script executable

--- .codebuild/setup.sh
+++ .codebuild/setup.sh
@@ -4,6 +4,10 @@ echo "Pulling Node image"
 
 docker pull node:8-alpine
 
+echo "Making sure permissions are correct"
+
+chmod a+x .codebuild/bin/*
+
 echo "Adding custom bin to path"
 export PATH=$PATH:$PWD/.codebuild/bin
 
