7d6d6724fef29b7407cd0320a338556b58101f4c
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-10-30 16:14:05 -0600

    get continuous deployment working

--- infrastructure/deploy-infrastructure.sh
+++ infrastructure/deploy-infrastructure.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-
 env=$1
 
+if [ ! -n "$env" ]; then
+  echo "Usage is deploy-infrastructure.sh (environment)"
+  exit 1
+fi
 dns_stack=web-community-cdn-dns-$env
 
 if [ "$env" = "prod" ]; then
@@ -11,6 +14,26 @@ fi
 
 packaged=/tmp/web-community-packaged-infrastructure-$(date +"%s").yml
 
+staging_bucket_or=byu-web-community-cdn-infra-staging-$env-us-west-2
+staging_bucket_va=byu-web-community-cdn-infra-staging-$env-us-east-1
+
+if ! aws s3api head-bucket --bucket $staging_bucket_or; then
+  echo "Bucket $staging_bucket_or does not exist; creating"
+  aws s3api create-bucket \
+    --bucket $staging_bucket_or \
+    --acl private \
+    --region us-west-2 \
+    --create-bucket-configuration LocationConstraint=us-west-2
+fi
+
+if ! aws s3api head-bucket --bucket $staging_bucket_va; then
+  echo "Bucket $staging_bucket_va does not exist; creating"
+  aws s3api create-bucket \
+    --bucket $staging_bucket_va \
+    --acl private \
+    --region us-east-1
+fi
+
 here=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 set -e
@@ -18,9 +41,11 @@ set -e
 aws cloudformation validate-template \
     --template-body file://$here/infrastructure.yml
 
+echo Packaging to s3://$staging_bucket_or
+
 aws cloudformation package \
     --template-file $here/infrastructure.yml \
-    --s3-bucket byu-web-community-cdn-infra-staging-$env \
+    --s3-bucket $staging_bucket_or \
     --output-template-file $packaged
 
 aws cloudformation deploy \
